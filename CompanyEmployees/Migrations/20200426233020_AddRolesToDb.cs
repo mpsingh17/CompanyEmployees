0f77c2256ba4e5e2632bee5402fef526b592c267
mpsingh17 <mpsingh17@outlook.co.nz>
2020-04-27 11:31:56 +1200

    seed roles.

---
+++ CompanyEmployees/Migrations/20200426233020_AddRolesToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CompanyEmployees.Migrations
+{
+    public partial class AddRolesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "3721f205-3777-46e4-858b-1f1bae2ed58e", "6c86774a-20d0-4550-80ed-29f2d1912c3a", "Manager", "MANAGER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "5cd272c1-769f-4e63-b141-329959d878bc", "1b939c33-454b-4ed0-b1b9-996911e9874b", "Admin", "ADMIN" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3721f205-3777-46e4-858b-1f1bae2ed58e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5cd272c1-769f-4e63-b141-329959d878bc");
+        }
+    }
+}
--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -20,6 +20,7 @@ namespace Entities
 
             modelBuilder.ApplyConfiguration(new CompanyConfiguration());
             modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
         }
 
         public DbSet<Employee> Employees { get; set; }
