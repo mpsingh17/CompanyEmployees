17cdb140650707be9f8abe5c0e9f64cb93b41643
mpsingh17 <mpsingh17@outlook.co.nz>
2020-04-27 16:30:46 +1200

    role based authorisation.

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -34,7 +34,7 @@ namespace CompanyEmployees.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet, Authorize]
+        [HttpGet, Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetCompanies()
         {
             var companies = await _repository.Company.GetAllCompaniesAsync(trackChanges: false);
